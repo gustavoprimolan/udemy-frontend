6ff8bc3f29ba608b520e0f1ab4a909303cd75a3d
Gustavo Primolan <gustavo.primolan@hotmail.com>
2021-05-20 15:17:54 -0300

    Updating react course

--- Modern React with Redux/videos-project/src/components/App.js
+++ Modern React with Redux/videos-project/src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
-import VideoList from './VideoList';
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -15,18 +20,31 @@ class App extends React.Component {
 
     const videos = response.data.items;
 
-    this.setState({videos: videos});
+    this.setState({ videos: videos, selectedVideo: videos[0] });
   };
 
   onVideoSelect = (video) => {
-    console.log('From the app: ', video);
+    console.log("From the app: ", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
